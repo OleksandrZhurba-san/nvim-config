2d31fa779a04d48b135a465262a4690d214a63ec
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2025-03-03 09:04:20 +0100

    color scheme ?

--- lua/deus/lazy/colorscheme.lua
+++ lua/deus/lazy/colorscheme.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or "tokyonight"
+  color = color or "gruber-darker"
   vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4" }) -- Set hover background and foreground
   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#f38ba8" }) -- Set hover border colorsend
@@ -7,14 +7,19 @@ end
 
 return {
   {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
+    "blazkowolf/gruber-darker.nvim",
+    -- name = "tokyonight",
     --"ellisonleao/gruvbox.nvim",
     --name = "gruvbox",
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
-        style = "night",
+      require("gruber-darker").setup({
+        opt = {
+          bold = false,
+          italic = {
+            strings = false,
+          }
+        }
       })
       ColorMyPencils()
     end,
